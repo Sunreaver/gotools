532016c22117c5d6f273283f6f208c5c665ef782
TheOne <tanwei.rush@gmail.com>
2016-03-16 14:19:33 +0800

    目录、文件是否存在判断

--- system.go
+++ system.go
@@ -24,3 +24,24 @@ func SystemSep() (path string) {
 	}
 	return path
 }
+
+//判断目录是否存在
+func IsDirExists(path string) bool {
+	fi, err := os.Stat(path)
+
+	if err != nil {
+		return os.IsExist(err)
+	} else {
+		return fi.IsDir()
+	}
+}
+
+//判断文件是否存在
+func IsFileExists(filePath string) bool {
+	fi, err := os.Open(filePath)
+	if err != nil {
+		return false
+	}
+	fi.Close()
+	return true
+}
