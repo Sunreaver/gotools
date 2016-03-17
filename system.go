21cddcf942c9ca4cdc818ff771ad1663dbc84264
TheOne <tanwei.rush@gmail.com>
2016-03-17 09:38:15 +0800

    规范注释

--- system.go
+++ system.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 )
 
-//获取当前运行目录
+// CurPath 获取当前运行目录
 func CurPath() (path string) {
 	file, _ := exec.LookPath(os.Args[0])
 	pt, _ := filepath.Abs(file)
@@ -14,7 +14,7 @@ func CurPath() (path string) {
 	return filepath.Dir(pt)
 }
 
-//获取系统分隔符
+// SystemSep 获取系统分隔符
 func SystemSep() (path string) {
 
 	if os.IsPathSeparator('\\') { //前边的判断是否是系统的分隔符
@@ -25,18 +25,17 @@ func SystemSep() (path string) {
 	return path
 }
 
-//判断目录是否存在
+// IsDirExists 判断目录是否存在
 func IsDirExists(path string) bool {
 	fi, err := os.Stat(path)
 
 	if err != nil {
 		return os.IsExist(err)
-	} else {
-		return fi.IsDir()
 	}
+    return fi.IsDir()
 }
 
-//判断文件是否存在
+// IsFileExists 判断文件是否存在
 func IsFileExists(filePath string) bool {
 	fi, err := os.Open(filePath)
 	if err != nil {
