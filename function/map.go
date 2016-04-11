46219e1868aa0af1d521adf9d40f1d74e5e046b0
TheOne <tanwei.rush@gmail.com>
2016-04-11 13:50:39 +0800

    map copy

---
+++ function/map.go
@@ -0,0 +1,12 @@
+package function
+
+import "reflect"
+
+// MapCopy 拷贝map
+func MapCopy(dst, src interface{}) {
+	dv, sv := reflect.ValueOf(dst), reflect.ValueOf(src)
+
+	for _, k := range sv.MapKeys() {
+		dv.SetMapIndex(k, sv.MapIndex(k))
+	}
+}
