13606ab2caede14d8ff6b99ff53e822d6754b9cc
Peter <tanwei.rush@gmail.com>
2017-04-27 16:19:01 +0800

    修改日志文件按日分割逻辑

--- logger/logger.go
+++ logger/logger.go
@@ -2,52 +2,145 @@ package logger
 
 import (
 	"os"
-	"path/filepath"
+	"path"
+	"sync"
 	"time"
 
-	"github.com/spf13/viper"
-	"github.com/sunreaver/gotools/system"
-	"github.com/uber-go/zap"
+	"github.com/pkg/errors"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-// Logger is zap json logger wrapper.
-var Logger zap.Logger
+type instance struct {
+	logger *zap.SugaredLogger
+	writer *lumberjack.Logger
+}
 
-func init() {
-	viper.AutomaticEnv()
-	viper.SetDefault("DEBUG", "0")
-	viper.SetDefault("LOGPATH", system.CurPath())
-	opts := getOptions()
-	Logger = zap.New(zap.NewJSONEncoder(zap.RFC3339Formatter("time")), opts...)
+type loggerMap struct {
+	lock      *sync.RWMutex
+	instances map[string]instance
+}
+
+var (
+	loggers = loggerMap{
+		new(sync.RWMutex),
+		make(map[string]instance),
+	}
+	directory string
+	level     zapcore.LevelEnabler
+	// Logger zap.Logger实例
+	Logger *zap.SugaredLogger
+)
+
+// InitLogger 初始化
+// path 输出路径
+// debugLevel 是否输出debug信息
+// location 日志文件名所属时区/默认东八区
+func InitLogger(path string, debugLevel bool, location *time.Location) {
+	directory = path
+	if debugLevel {
+		level = zap.DebugLevel
+	} else {
+		level = zap.InfoLevel
+	}
+	//Fix time offset for Local
+	// lt := time.FixedZone("Asia/Shanghai", 8*60*60)
+	if location == nil {
+		location = time.FixedZone("Asia/Shanghai", 8*60*60)
+	}
+	time.Local = location
+
+	Logger = GetLogger(time.Now().Format("2006-01-02"))
 
 	go func() {
+		lastFile := time.Now().Format("2006-01-02")
 		for {
-			time.Sleep(time.Second * time.Duration(86400-(time.Now().Unix()%86400)))
-			opts := getOptions()
-			Logger = zap.New(zap.NewJSONEncoder(zap.RFC3339Formatter("time")), opts...)
+			time.Sleep(time.Minute)
+			if lastFile != time.Now().Format("2006-01-02") {
+				lastFile = time.Now().Format("2006-01-02")
+				Logger = GetLogger(lastFile)
+			}
 		}
 	}()
 }
 
-func getFile() *os.File {
-	path := viper.GetString("LOGPATH")
-	fn := time.Now().Format("2006-01-02") + ".log"
-	fp := filepath.Join(path, fn)
-	f, err := os.OpenFile(fp, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		panic(err)
-	}
-	return f
+// BeiJingTimeFormatter encodes the entry time as an RFC3339-formatted string under
+// the provided key.
+// func BeiJingTimeFormatter(key string) zap.TimeFormatter {
+// 	return zap.TimeFormatter(func(t time.Time) zap.Field {
+// 		return zap.String(key, t.Local().Format(time.RFC3339))
+// 	})
+// }
+
+func localTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	t = t.Local()
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
 }
 
-func getOptions() []zap.Option {
-	var opts []zap.Option
-	if viper.GetString("DEBUG") != "0" {
-		opts = append(opts, zap.DebugLevel)
-	} else {
-		opts = append(opts, zap.InfoLevel)
+func (l *loggerMap) Get(name string) *zap.SugaredLogger {
+	i, ok := l.instances[name]
+	if !ok {
+		l.lock.Lock()
+		i, ok = l.instances[name]
+		if !ok {
+			writer := &lumberjack.Logger{
+				Filename: path.Join(directory, name+".log"),
+			}
+			ws := zapcore.AddSync(writer)
+			cfg := zapcore.EncoderConfig{
+				TimeKey:        "time",
+				LevelKey:       "level",
+				NameKey:        "logger",
+				CallerKey:      "caller",
+				MessageKey:     "message",
+				StacktraceKey:  "stacktrace",
+				EncodeLevel:    zapcore.LowercaseLevelEncoder,
+				EncodeTime:     localTimeEncoder,
+				EncodeDuration: zapcore.NanosDurationEncoder,
+			}
+			logger := zap.New(zapcore.NewCore(
+				zapcore.NewJSONEncoder(cfg),
+				ws,
+				level,
+			))
+			i = instance{
+				logger: logger.Sugar(),
+				writer: writer,
+			}
+			l.instances[name] = i
+		}
+		defer l.lock.Unlock()
 	}
-	f := getFile()
-	opts = append(opts, zap.Output(f))
-	return opts
+	return i.logger
+}
+
+// RotateLog to causes Logger to close the existing log file
+// and immediately create a new one.
+func RotateLog() {
+	loggers.lock.RLock()
+	for _, i := range loggers.instances {
+		i.writer.Rotate()
+	}
+	loggers.lock.RUnlock()
+}
+
+func exists(path string) error {
+	stat, err := os.Stat(path)
+	if err == nil {
+		return errors.Wrap(err, "directory")
+	}
+	if os.IsNotExist(err) {
+		return errors.New("path is not exists: " + path)
+	}
+	if !stat.IsDir() {
+		return errors.New("path is not directory: " + path)
+	}
+	return err
+}
+
+// GetLogger to get zap.SugaredLogger
+func GetLogger(name string) *zap.SugaredLogger {
+	return loggers.Get(name)
 }
