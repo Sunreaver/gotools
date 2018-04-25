5e76c2a118fe77623f2ed945c5f1596a11bb69b1
weit <weit@jumei.com>
2018-04-25 10:47:22 +0800

    change return data format

--- http/http.go
+++ http/http.go
@@ -1,9 +1,7 @@
 package http
 
 import (
-	"errors"
 	"io"
-	"io/ioutil"
 	h "net/http"
 	"net/url"
 )
@@ -11,7 +9,7 @@ import (
 // Get 获取url对应的文件内容
 // header: header 请求体的header map,例如Content-Type:application/json
 // 返回到content中
-func Get(uri string) (response *Resp, e error) {
+func Get(uri string) (response Response, e error) {
 	client := &h.Client{}
 	resp, err := client.Get(uri)
 	if err != nil {
@@ -24,7 +22,7 @@ func Get(uri string) (response *Resp, e error) {
 // uri: post到的地址
 // header: header 请求体的header map,例如Content-Type:application/json
 // body: post的数据
-func Post(uri string, header map[string]string, body io.Reader) (response *Resp, err error) {
+func Post(uri string, header map[string]string, body io.Reader) (response Response, err error) {
 	req, err := h.NewRequest(h.MethodPost, uri, body)
 	for key, item := range header {
 		req.Header.Add(key, item)
@@ -40,7 +38,7 @@ func Post(uri string, header map[string]string, body io.Reader) (response *Resp,
 // PostForm will post form数据
 // uri: post到的地址
 // body: form数据
-func PostForm(uri string, data url.Values) (response *Resp, err error) {
+func PostForm(uri string, data url.Values) (response Response, err error) {
 	client := &h.Client{}
 	resp, err := client.PostForm(uri, data)
 	if err != nil {
@@ -53,7 +51,7 @@ func PostForm(uri string, data url.Values) (response *Resp, err error) {
 // uri: post到的地址
 // header: header 请求体的header map,例如Content-Type:application/json
 // body: post的数据
-func Put(uri string, header map[string]string, body io.Reader) (response *Resp, err error) {
+func Put(uri string, header map[string]string, body io.Reader) (response Response, err error) {
 	req, err := h.NewRequest(h.MethodPut, uri, body)
 	for key, item := range header {
 		req.Header.Add(key, item)
@@ -70,7 +68,7 @@ func Put(uri string, header map[string]string, body io.Reader) (response *Resp, 
 // uri: post到的地址
 // header: header 请求体的header map,例如Content-Type:application/json
 // body: post的数据
-func Delete(uri string, header map[string]string, body io.Reader) (response *Resp, err error) {
+func Delete(uri string, header map[string]string, body io.Reader) (response Response, err error) {
 	req, err := h.NewRequest(h.MethodDelete, uri, body)
 	for key, item := range header {
 		req.Header.Add(key, item)
@@ -81,20 +79,4 @@ func Delete(uri string, header map[string]string, body io.Reader) (response *Res
 		return nil, err
 	}
 	return getRespBody(resp)
-}
-
-func getRespBody(resp *h.Response) (response *Resp, e error) {
-	if resp == nil {
-		return nil, errors.New("response is nil")
-	}
-	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return &Resp{
-		header:  resp.Header,
-		content: content,
-		code:    resp.StatusCode,
-	}, nil
 }
--- http/response.go
+++ http/response.go
@@ -1,6 +1,10 @@
 package http
 
-import h "net/http"
+import (
+	"errors"
+	"io/ioutil"
+	h "net/http"
+)
 
 // Resp http response
 type Resp struct {
@@ -31,4 +35,20 @@ func (r *Resp) GetStatusCode() int {
 		return r.code
 	}
 	return 0
+}
+
+func getRespBody(resp *h.Response) (response *Resp, e error) {
+	if resp == nil {
+		return nil, errors.New("response is nil")
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return &Resp{
+		header:  resp.Header,
+		content: content,
+		code:    resp.StatusCode,
+	}, nil
 }
